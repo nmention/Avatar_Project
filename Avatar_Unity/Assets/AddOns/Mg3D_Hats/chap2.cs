50b99896706b15931f448130dc0902ceec26a9cb
nmention <noe.mention@protonmail.com>
2024-01-15 13:18:56 +0100

    implemented smooth change of hats

--- Avatar_Unity/Assets/AddOns/Mg3D_Hats/chap2.cs
+++ Avatar_Unity/Assets/AddOns/Mg3D_Hats/chap2.cs
@@ -2,24 +2,23 @@ using UnityEngine;
 
 public class ToggleChapeau : MonoBehaviour
 {
-    public GameObject Hat1; // Référence à l'objet à activer/désactiver
+    public GameObject Hat1; // Rï¿½fï¿½rence ï¿½ l'objet ï¿½ activer/dï¿½sactiver
     public GameObject Hat2;
     public static int M=0;
-    // Fonction pour activer/désactiver l'objet
+
+    
+    // Fonction pour activer/dï¿½sactiver l'objet
     public void changement()
     {
-        if (M == 0) { 
+        if (M % 2 == 0) { 
             Hat1.SetActive(!Hat1.activeSelf); 
         }
-        if (M == 1)
+        if (M % 2 ==  1)
         {
             Hat1.SetActive(!Hat1.activeSelf);
             Hat2.SetActive(!Hat2.activeSelf);
         }
-        if(M == 2)
-        {
-            Hat2.SetActive(!Hat2.activeSelf);
-        }
+        
         M++;
     }
 }
