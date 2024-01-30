323801c83df0b8b9f831f07f5c694aa926fbd9e3
nmention <noe.mention@protonmail.com>
2024-01-30 16:34:08 +0100

    start of save / export feature

---
+++ Avatar_Unity/Assets/Backend/Save.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Save : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string avatarName;
+
+    public List<GameObject> accessories;
+    public GameObject avatar;
+
+
+    public Save()
+    {
+        avatarName = avatar.name;
+        accessories = new List<GameObject>();
+        foreach (GameObject item in accessories)
+        {
+            this.accessories.Add(item);
+        }
+    }
+    
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void exportAvatar()
+    {
+        Save save = new Save();
+        string jsonData = JsonUtility.ToJson(avatar);
+        
+
+    }
+}
